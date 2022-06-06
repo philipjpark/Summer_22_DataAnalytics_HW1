3c226a20ded23d450a749a70755cc30cd8b172b8
philipjpark <66290921+philipjpark@users.noreply.github.com>
2022-06-06 12:32:04 -0400

    HW Submission

--- Factorial_Digit_Sum.py
+++ Factorial_Digit_Sum.py
@@ -1,5 +1,4 @@
 n = 100
-
 fact = 1
 
 if n < 0:
@@ -9,12 +8,8 @@ elif n == 0:
 else:
   for i in range(1,n + 1):
     fact = fact*i
-  #print("The factorial of",num,"is",factorial)
-#print(str(factorial))
 
 arr = [int(x) for x in str(fact)]
-#print(str(arr))
-#print(arr)
 
 sum = 0
 for z in arr:
